9d0dfd94dd9ccded75d0fd6f5edcc0dacd7cafbf
wacero <acerowilson@gmail.com>
2021-09-30 15:36:26 -0500

    correct license in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
      install_requires = ['obspy>=1.1.0','python-speech-features>=0.6','sympy>=1.8'],
      classifiers=[
          "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: CeCILL",
+         "License :: CeCILL-B Free Software License Agreement (CECILL-B)",
          "Operating System :: OS Independent",
      ],
  )
