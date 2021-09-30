eb837ea69c9a630a103bf5570d0e6b1e0d595077
wacero <acerowilson@gmail.com>
2021-09-30 14:02:04 -0500

    added all needed files

---
+++ call_aaa_features.py
@@ -0,0 +1,24 @@
+
+from obspy import read
+import sys
+sys.path.append("./aaa_features")
+from features import FeatureVector
+
+data_mseed=read("./data_sample/EC.RETU..SHZ.2012.06.28.185423.mseed")
+data = data_mseed[0].data
+# Change if you want your screen to keep quiet
+# 0 = quiet
+# 1 = in between
+# 2 = detailed information
+verbatim = 2
+
+config = {'features_file':'./config_sample/features_00.json', 'domains':'time spectral cepstral'}
+
+# Feature extraction for each data
+features = FeatureVector(config, verbatim=verbatim)
+
+features.compute(data,100)
+for i,f in enumerate(features.featuresValues):
+    print(i,f)
+            
+            
